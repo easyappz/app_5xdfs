6e924d47854517fa347fe73c2c11819f69a7bd73
IbragimKhaniev <newinterny@gmail.com>
2025-11-23 15:05:51 +0300

    Add Member and ChatMessage models with serializers, migration and OpenAPI schemas

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ChatMessage, Member
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "created_at")
+    search_fields = ("username",)
+    ordering = ("id",)
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "member", "short_content", "created_at")
+    list_select_related = ("member",)
+    search_fields = ("content", "member__username")
+    ordering = ("created_at",)
+
+    def short_content(self, obj):
+        content = obj.content
+        if len(content) > 50:
+            return f"{content[:47]}..."
+        return content
+
+    short_content.short_description = "content"
--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,46 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
+
+from .models import ChatMessage, Member
 
 
 class MessageSerializer(serializers.Serializer):
     message = serializers.CharField(max_length=200)
     timestamp = serializers.DateTimeField(read_only=True)
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        fields = ("id", "username", "created_at")
+        read_only_fields = ("id", "username", "created_at")
+
+
+class MemberRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, max_length=128)
+
+    class Meta:
+        model = Member
+        fields = ("username", "password")
+
+    def validate_username(self, value):
+        if Member.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "A member with this username already exists.",
+            )
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        hashed_password = make_password(password)
+        member = Member.objects.create(password=hashed_password, **validated_data)
+        return member
+
+
+class ChatMessageSerializer(serializers.ModelSerializer):
+    member = MemberSerializer(read_only=True)
+
+    class Meta:
+        model = ChatMessage
+        fields = ("id", "content", "created_at", "member")
+        read_only_fields = ("id", "created_at", "member")
